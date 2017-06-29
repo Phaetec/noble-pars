34b587af85dfe48078cd09b48ed5aa67f534b7d6
Alexander Schneider <alexander@schneider.gg>
2017-06-29 12:42:21 +0200

    Add keys-to-string function

--- src/noble/translator.clj
+++ src/noble/translator.clj
@@ -1,9 +1,17 @@
-(ns noble.translator
-  (:require [clojure.walk :refer [stringify-keys]]))
+(ns noble.translator)
 
 (defn create-graphql
   "Takes clojure data and outputs a valid graphql query-string"
   [data]
   data)
 
-
+(defn keys-to-string
+  "Takes a collection and converts all keywords to strings."
+  [coll]
+  (map 
+   #(if (coll? %)
+      (keys-to-string %)
+      (if (keyword? %)
+       (name %)
+       %))
+   coll))
