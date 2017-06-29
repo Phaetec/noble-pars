fc75f105570a47b05c40a56992cad0c179e93991
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-06-29 21:36:57 +0200

    Format code, change fn name, update docstrings

--- src/noble/translator.clj
+++ src/noble/translator.clj
@@ -3,7 +3,7 @@
 (defn keys-to-string
   "Takes a collection and converts all keywords to strings."
   [coll]
-  (map 
+  (map
    #(if (coll? %)
       (keys-to-string %)
       (if (keyword? %)
@@ -12,17 +12,17 @@
    coll))
 
 (defn collection-to-string
-  "Takes a prepared collection of statements and converts them into graphql syntax"
+  "Takes a prepared collection of statements and converts them into graphql syntax."
   [coll]
   (str "{" (apply str (map #(str " "(if (coll? %)
                                       (collection-to-string %)
                                       (str %)))
                            coll)) "}"))
 
-(defn create-graphql
-  "Takes clojure data and outputs a valid graphql query-string"
+(defn edn->graphql
+  "Takes clojure data and outputs a valid graphql query-string."
   [data]
   (->> data
-      keys-to-string
-      collection-to-string
-      (str "query ")))
+       keys-to-string
+       collection-to-string
+       (str "query ")))
