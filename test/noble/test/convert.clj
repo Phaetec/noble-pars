b3c2c1270ec050b503e6d678f126cc9f95dedbb1
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-06-29 23:07:25 +0200

    Tidy up and fix tests

--- test/noble/test/convert.clj
+++ test/noble/test/convert.clj
@@ -1,16 +1,16 @@
-(ns noble.test.translator
+(ns noble.test.convert
   (:require [clojure.test :refer [deftest is are testing]]
             [clojure.spec.test.alpha :as stest]
-            [noble.translator :as translator]))
+            [noble.convert :as convert]))
 
 (deftest vec->str
   (testing "Convert vector to string."
     (are [x y] (= x y)
-      "iamgroot" (translator/vec->str [:iamgroot])
-      "i amgroot" (translator/vec->str [:i :amgroot])
-      "i am groot" (translator/vec->str [:i :am :groot]))))
+      "iamgroot" (convert/vec->str [:iamgroot])
+      "i amgroot" (convert/vec->str [:i :amgroot])
+      "i am groot" (convert/vec->str [:i :am :groot]))))
 
 (deftest convert-type
   (are [x y] (= x y)
-    "whoami { name }" (translator/convert-type {:whoami [:name]})
-    "razupaltuff { kangaroo penguin }" (translator/convert-type {:razupaltuff [:kangaroo :penguin]})))
+    "whoami { name }" (convert/convert-type {:whoami [:name]})
+    "razupaltuff { kangaroo penguin }" (convert/convert-type {:razupaltuff [:kangaroo :penguin]})))
