c808ff623470d3e8920d60b0a943434f4ad79ea9
Alexander Schneider <alexander@schneider.gg>
2017-06-29 12:19:41 +0200

    add translator class

---
+++ src/noble/translator.clj
@@ -0,0 +1,9 @@
+(ns noble.translator
+  (:require [clojure.walk :refer [stringify-keys]]))
+
+(defn create-graphql
+  "Takes clojure data and outputs a valid graphql query-string"
+  [data]
+  data)
+
+
