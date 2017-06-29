f4062b6bd15aad7d86e9ffa4ea67a1a747c49906
Christian Meter <christian.meter@uni-duesseldorf.de>
2017-06-29 21:34:26 +0200

    Update dependencies; add dev deps

--- project.clj
+++ project.clj
@@ -3,4 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]]
+  :plugins [[lein-ancient "0.6.10"]
+            [lein-kibit "0.1.5"]
+            [jonase/eastwood "0.2.4"]]
+  :profiles {:dev {:dependencies [[org.clojure/data.json "0.2.6"]
+                                  [clj-http "3.6.1"]]}})
